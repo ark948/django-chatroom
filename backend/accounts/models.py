6c10bf5d76ceea91ee4bac632d00a13d2962255e
ark948 <armankarimi948@gmail.com>
2025-01-09 23:26:11 +0330

    Custom User model added

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
