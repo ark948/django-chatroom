b96e4f35b877f59fa3ce268be78a8726cea7cf47
ark948 <armankarimi948@gmail.com>
2025-01-11 22:35:02 +0330

    Initiated pytest successfully

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import pytest
 
 # Create your tests here.
+
+from accounts.models import CustomUser
+
+
+@pytest.mark.django_db
+def test_user_create():
+    CustomUser.objects.create_user('test01', 'test01@something.com', 'test123*A')
+    assert CustomUser.objects.count() == 1
