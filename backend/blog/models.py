de074baef3a14ea2e28b9f16629ba14a0e09f10b
ark948 <armankarimi948@gmail.com>
2025-01-18 20:06:10 +0330

    Fixed problem of blog post model and added markdown

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
+from django.contrib.auth import get_user_model
 
 
 
 # Create your models here.
 
+class TimeStampModel(models.Model):
+    """Abstract base class that adds created_at and updated_at fields to models."""
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True # this model is abstract, do not create table for it
 
 
 
-class Post(models.Model):
+
+class Post(TimeStampModel):
     title = models.CharField('Title', max_length=200)
     body = CKEditor5Field('Body', config_name='extends')
-    author = models.OneToOneField('accounts.CustomUser', on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"<Post Blog object {self.title}>"
