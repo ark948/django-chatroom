2d1a11709f5852e4980fb9f16069c65c80d6c068
ark948 <armankarimi948@gmail.com>
2025-02-11 14:55:34 +0330

    Starting over...

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.utils.translation import gettext as _
+
 
 # Create your views here.
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request=request, template_name='home/index.html')
+
+
+def test_translation(request: HttpRequest) -> HttpResponse:
+    output = _("Welcome to this site.")
+    return HttpResponse(output)
